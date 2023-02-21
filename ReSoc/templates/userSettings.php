886633a9768de65869d069eefe191004058d218a
HeleneVeber <veber.helene@gmail.com>
2023-02-21 09:59:49 +0100

    Co-authored-by: Nastiakor <Nastiakor@users.noreply.github.com> Co-authored-by: yileon-ada <yileon-ada@users.noreply.github.com>

---
+++ ReSoc/templates/userSettings.php
@@ -0,0 +1,26 @@
+<?php $title = "ReSoC - Paramètres"; ?>
+<?php $pageDescription = "les informations de"; ?>
+
+<?php ob_start(); ?>
+                           
+<main>
+    <article class='parameters'>
+        <h3>Mes paramètres</h3>
+        <dl>
+            <dt>Pseudo</dt>
+            <dd><?= $user['alias'] ?></dd>
+            <dt>Email</dt>
+            <dd><?= $user['email'] ?></dd>
+            <dt>Nombre de messages</dt>
+            <dd><?= $user['totalpost'] ?></dd>
+            <dt>Nombre de "J'aime" donnés </dt>
+            <dd><?= $user['totalgiven'] ?></dd>
+            <dt>Nombre de "J'aime" reçus</dt>
+            <dd><?= $user['totalrecieved'] ?></dd>
+        </dl>
+    </article>
+</main>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require 'layout.php'; ?>
---
+++ ReSoc/templates/userWall.php
@@ -0,0 +1,44 @@
+<?php $title = "ReSoC - Mur"; ?>
+<?php $pageDescription = "tous les messages de"; ?>
+
+<?php ob_start(); ?>
+                           
+<main>
+    <?php
+    foreach ($posts as $post) {
+    ?>
+        <article>
+            <h3>
+                <time datetime=<?= $post['created']?> >
+                    <?php $date = new DateTime($post['created']); 
+                    echo $date->format('d F Y à H:i');
+                    ?> 
+                </time>
+            </h3>
+            
+            <address><?= $post['alias'] ?></address>
+            
+            <div>
+                <p><?= $post['content'] ?></p>
+            </div>
+            
+            <footer>
+                <small>♥<?= $post['likes'] ?></small>
+                <?php 
+                    $tag = $post['taglist'];
+                    $arrayOfTags = explode(",",$tag);
+                    $index = 0;
+                    for ($index = 0; $index < count($arrayOfTags); $index++) {
+                        echo '<a href="">' . "#" . $arrayOfTags[$index] . '</a>' . ' ';
+                    }
+                ?>
+            </footer>
+        </article>
+    <?php
+    }
+    ?>
+</main>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require 'layout.php'; ?>
